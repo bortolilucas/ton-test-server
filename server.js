6b6392a0eba018a3886cf824442ef2913c7214b2
Lucas Bortoli <lucasbortolidev@gmail.com>
2021-12-05 00:19:20 -0300

    chore: add heroku setup

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const apiRoutes = require('./routes/api');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use('/api', apiRoutes);
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(port, () => console.log('Server running on port 3000'));
