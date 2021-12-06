14c4ca3a38279c6a23a58953975ff1ca717fdc8e
Lucas Bortoli <lucasbortolidev@gmail.com>
2021-12-06 16:37:45 -0300

    feat(api): add simple search by title to products endpoint

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 const products = require('../data/products.json');
-const { paginateResults } = require('../middlewares/pagination');
 
-router.get('/products', paginateResults(products), (_, res) => {
-  res.json(res.paginatedResults);
+router.get('/products', (req, res) => {
+  let results = products;
+
+  const search = req.query.search;
+
+  if (search) {
+    results = results.filter((a) => a.title.includes(search));
+  }
+
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 15;
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  res.json({
+    products: results.slice(startIndex, endIndex),
+    previous: startIndex > 0 ? page - 1 : null,
+    current: page,
+    next: endIndex < results.length ? page + 1 : null,
+    limit,
+  });
 });
 
 module.exports = router;
